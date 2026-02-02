f20d7c94376a378168a6a21feccf8fd494cd853c
abhay9876 <singhabhayrna987@gmail.com>
2026-02-02 11:21:40 +0530

    [Abhay Singh] Add.Employee Class file

---
+++ LambdaLinq/Employee/Employees.cs
@@ -0,0 +1,12 @@
+namespace Employee
+{
+    public class Employees
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public DateTime Date { get; set; }
+
+
+
+    }
+}
