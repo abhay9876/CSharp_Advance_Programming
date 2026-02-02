0046a53f8027cceb6f419ee5ba12f5084eaaa649
abhay9876 <singhabhayrna987@gmail.com>
2026-02-02 10:51:28 +0530

    [Abhay Singh] Add. Calculator logic file

---
+++ NUnit/Core/Calculator.cs
@@ -0,0 +1,45 @@
+﻿/*Basic NUnit Test: Testing a Calculator Class
+Problem:
+Create a Calculator class with methods:
+● Add(int a, int b)
+● Subtract(int a, int b)
+● Multiply(int a, int b)
+● Divide(int a, int b)
+Write NUnit or MSTest test cases for each method.
+ */
+
+
+
+
+
+
+
+namespace Core;
+
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Divide(int a, int b)
+    {
+        if (b == 0)
+            throw new DivideByZeroException();
+
+        return a / b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+}
+
