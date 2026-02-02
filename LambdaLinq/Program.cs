ceeedacc6071c53bdafc19b503724ebc8962e35c
abhay9876 <singhabhayrna987@gmail.com>
2026-02-02 11:25:02 +0530

    [Abhay Singh] Add. Lambda Linq problems

---
+++ LambdaLinq/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Employee;
+using SortList;
+using Products;
+
+namespace LambdaLinq
+{
+    class Program
+    {
+        public static void Main()
+        {
+            EmployeeRecords.FilterRecords();
+
+            Sorting.GetSort();
+
+            ManageProducts.GroupProducts();
+        }
+    }
+}
